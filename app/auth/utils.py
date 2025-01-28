03495280fa38e9e62ecc320f3274c3b424a314c9
bernardoraos90 <bernardoraos90@gmail.com>
2025-01-28 16:21:25 -0300

    asdasd123

--- app/auth/utils.py
+++ app/auth/utils.py
@@ -3,6 +3,7 @@ from typing import Optional
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer 
 from sqlalchemy.orm import Session
 import sys
 import traceback
