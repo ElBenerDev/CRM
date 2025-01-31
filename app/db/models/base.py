161f14bafd5d4d2ae6a2b1bec70a6e2557074666
bernardoraos90 <bernardoraos90@gmail.com>
2025-01-31 18:07:00 -0300

    change1

--- app/db/models/base.py
+++ app/db/models/base.py
@@ -1,5 +1,11 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, DateTime
+from sqlalchemy import Column, DateTime, Integer
 from datetime import datetime, timezone
 
 Base = declarative_base()
+
+class TimestampedModel:
+    """Mixin que agrega campos de timestamp a los modelos"""
+    id = Column(Integer, primary_key=True, index=True)
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
