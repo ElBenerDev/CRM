42947dcff336cbda7b9625fc480e0bca5ca6489d
bernardoraos90 <bernardoraos90@gmail.com>
2025-02-01 12:10:07 -0300

    svhj67

--- app/db/base.py
+++ app/db/base.py
@@ -1,4 +1,5 @@
 from app.db.base_class import Base  # noqa
 from app.db.models.appointment import Appointment  # noqa
 from app.db.models.patient import Patient  # noqa
+from app.db.models.lead import Lead  # noqa
 # Importa aquí todos tus modelos
